edbb7b5040ee385f2c4f2cc4874a5ce0d7af2e91
CalvinWilkinson <kinsondigital@gmail.com>
2025-01-01 14:30:41 +0000

    chore: complete empty template setup

---
+++ VelaptorTemplates/Templates/VelEmpty/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace VelEmpty;
+
+using Velaptor.UI;
+
+/// <summary>
+/// The main game class.
+/// </summary>
+public class Game : Window
+{
+	// Override various game loop methods here
+}
--- VelaptorTemplates/Templates/VelEmpty/Program.cs
+++ VelaptorTemplates/Templates/VelEmpty/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VelEmpty;
+
+// Create a new game instance and run it to start the game
+var game = new Game();
+game.Show();
